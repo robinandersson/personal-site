91541cab3dd60e1602e7849ec7e0ae6d67ee0321
Robin Andersson <k.robin.andersson@gmail.com>
2017-07-06 21:25:58 +0200

    add pretty permalinks

--- build.js
+++ build.js
@@ -3,6 +3,8 @@ var markdown = require('metalsmith-markdown');
 var layouts = require('metalsmith-layouts');
 var handlebars = require('handlebars');
 var collections = require('metalsmith-collections');
+var permalinks = require('metalsmith-permalinks');
+
 
 metalsmith(__dirname)
   .metadata({
@@ -20,6 +22,10 @@ metalsmith(__dirname)
       sortBy: 'date',
       reverse: true
     },
+  }))
+  .use(permalinks({
+    relative: false,
+    pattern: ':title',
   }))
   .use(layouts({
     engine: 'handlebars',
