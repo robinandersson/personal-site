c7b17b3313002dd94d576d501adad82ac8a8d98f
Robin Andersson <k.robin.andersson@gmail.com>
2017-07-09 10:50:25 +0200

    fix: correcly name and build scss partials

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,12 @@ var sourcemaps = require('gulp-sourcemaps');
 var build = require('./build.js');
 
 var path = {
-  mainScss: 'src/assets/stylesheets/site.scss',
   scss: 'src/assets/stylesheets/**/*.scss',
   html: 'layouts/**/*.html'
 }
 
 function processScss() {
-  return gulp.src(path.mainScss)
+  return gulp.src(path.scss)
     .pipe(sourcemaps.init())
     .pipe(sass({
       includePaths: [ './src/assets/stylesheets' ],
